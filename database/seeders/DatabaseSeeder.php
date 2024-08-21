20931980a162b262599450fe60631cf5884b7f7d
Keisuke Hayashi <mj66012@e.hiroshima-cu.ac.jp>
2024-08-21 14:15:32 +0900

    Revert "Revert "完成はしていないが一時的なコミット""
    This reverts commit 33185b9436f57f46d8479cea256bc52dd3344fc7.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use \App\Models\User;
+use \App\Models\Post;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,6 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(AcceptanceSeeder::class);
+
+        $this->call(TagSeeder::class);
+        $test = User::factory(10)->create();
+        $test = Post::factory()->create();
+        // $test = Acceptance::factory()->create();
+        // $test = Post_tag::factory()->create();
     }
 }
