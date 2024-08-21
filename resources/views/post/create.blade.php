b7ca9a4d6302bf5a4492ee47e0c16be5c0609fcf
zhouyunhao-mac <yunhaozhou@hiroshima-u.ac.jp>
2024-08-21 17:39:21 +0900

    Update create.blade.php

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -10,14 +10,44 @@
             <div class="bg-white shadow p-6 rounded-lg">
                 <form action="{{ route('post.store') }}" method="post">
                     @csrf
+                    <!-- タイトル -->
                     <div class="mb-4">
                         <label for="title" class="block text-gray-700 text-sm font-bold mb-2">タイトル</label>
-                        <input type="text" name="title" id="title" class="w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-500" required>
+                        <input type="text" name="title" id="title" maxlength="10" class="w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-500" required>
                     </div>
+                    <!-- 本文 -->
                     <div class="mb-4">
                         <label for="body" class="block text-gray-700 text-sm font-bold mb-2">本文</label>
-                        <textarea name="body" id="body" rows="6" class="w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-500" required></textarea>
+                        <textarea name="body" id="body" rows="6" maxlength="200" class="w-full  border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-500" style="resize: none;" required></textarea>
                     </div>
+                    <!-- 報酬 -->
+                    <div class="mb-4">
+                        <label for="reward" class="block text-gray-700 text-sm font-bold mb-2">報酬</label>
+                        <div class="flex items-center">
+                            <textarea name="reward" id="reward" rows="1" class="w-[10%] border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-500" style="resize: none;" required></textarea>
+                            <span class="ml-2">円</span>
+                        </div>
+                    </div>
+                    <!-- タグ -->
+                    <div class="mb-4">
+                        <label label for="tag_name" class="block text-gray-700 text-sm font-bold mb-2">タグ</label>
+                        <select name="tag_name" id="tag_name">
+                            <option value="1">手助け</option>
+                            <option value="2">探し物</option>
+                            <option value="3">その他</option>
+                        </select>
+                    </div>
+                    <!-- 住所 -->
+                    <div class="mb-4">
+                        <label for="address" class="block text-gray-700 text-sm font-bold mb-2">住所</label>
+                        <input type="text" name="address" id="address" class="w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-500" required>
+                    </div>
+                    <!-- 期限 -->
+                    <div class="mb-4" style="width: 180px;">
+                        <label for="deadline" class="block text-gray-700 text-sm font-bold mb-2">期限</label>
+                        <input type="datetime-local" name="deadline" id="deadline" class="w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-500">
+                    </div>
+                    <!-- ボタン -->
                     <div class="flex justify-end">
                         <button type="submit" class="py-2 px-4 btn btn-primary">投稿する</button>
                         <a href="{{ route('post.index') }}" class="py-2 px-4 ml-4 btn btn-secondary">キャンセル</a>
