893083679478b39b1d600ae656e1df300525c573
zhouyunhao-mac <yunhaozhou@hiroshima-u.ac.jp>
2024-08-21 09:40:43 +0900

    Update home.blade.php & Add google map

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            トップページ
+            Real Quest
+            <br>
+            リアル・クエスト
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <a href="{{ route('post.index') }}" class="bg-white border-b border-gray-200 p-6 block w-full text-center
-                font-semibold text-gray-800 hover:bg-gray-100 text-decoration-none">
-                    掲示板へ移動
-                </a>
-            </div>
-        </div>
+    <!-- Google map -->
+    <div id="map" class="h-[840px] w-[80%] ml-auto">
     </div>
+    <script src="{{ asset('/js/map.js') }}"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key=AIzaSyATCiHn-q2ufKqlPMJaRqCvdQoK3_5zJk0&callback=initMap" async defer></script>
+
 </x-app-layout>
